b6264bf93f5c3ec9f50372bcc174ded6396f8acb
Eltos <eltos@outlook.de>
2021-12-18 02:55:15 +0100

    Version 3.10

--- PasteIntoFile/Properties/AssemblyInfo.cs
+++ PasteIntoFile/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("Paste Clipboard Contents Into Files")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("PasteIntoFile")]
-[assembly: AssemblyProduct("PasteIntoFile v2.0")]
+[assembly: AssemblyProduct("PasteIntoFile")]
 [assembly: AssemblyCopyright("Copyright © PasteIntoFile GitHub contributors")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.9")]
-[assembly: AssemblyFileVersion("3.9")]
+[assembly: AssemblyVersion("3.10")]
+[assembly: AssemblyFileVersion("3.10")]
